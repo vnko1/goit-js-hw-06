ec9f2ba0524a53ad1550f77b0b18f55f6821cacd
Andrey Valenko <andreyvalenko@gmail.com>
2023-01-09 17:05:17 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -3,9 +3,7 @@ console.log(
   document.querySelectorAll(".item").length
 );
 
-const categories = document.querySelectorAll(".item");
-
-for (const category of categories) {
+document.querySelectorAll(".item").forEach((category) => {
   console.log("Category: ", category.querySelector("h2").textContent);
   console.log("Elements: ", category.querySelectorAll("li").length);
-}
+});
