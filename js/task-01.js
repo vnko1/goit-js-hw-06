d46e5cc3dbede21d397100d1b743d145f1638f2b
Andrey Valenko <andreyvalenko@gmail.com>
2023-01-09 17:08:28 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,8 @@
-console.log(
-  "Number of categories: ",
-  document.querySelectorAll(".item").length
-);
+const categories = document.querySelectorAll(".item");
 
-document.querySelectorAll(".item").forEach((category) => {
+console.log("Number of categories: ", categories.length);
+
+categories.forEach((category) => {
   console.log("Category: ", category.querySelector("h2").textContent);
   console.log("Elements: ", category.querySelectorAll("li").length);
 });
