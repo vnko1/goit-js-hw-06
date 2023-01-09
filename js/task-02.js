32b6c30d99ec8ccaa2d13df4134fb41eded7f8c0
Andrey Valenko <andreyvalenko@gmail.com>
2023-01-09 22:42:17 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,37 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const indredientsListEl = document.querySelector("#ingredients");
+
+function markingList(args) {
+  return args.map((ingredient) => {
+    const itemEl = document.createElement("li");
+    itemEl.textContent = ingredient;
+    itemEl.classList.add("item");
+    return itemEl;
+  });
+}
+
+indredientsListEl.append(...markingList(ingredients));
+
+//!-------------------------------------------------------------------------
+// const indredientsListEl = document.querySelector("#ingredients");
+
+// function markingList(args) {
+//   const arr = [];
+//   for (let i = 0; i < args.length; i++) {
+//     const itemEl = document.createElement("li");
+//     itemEl.textContent = args[i];
+//     itemEl.classList.add("item");
+//     arr.push(itemEl);
+//   }
+//   return arr;
+// }
+
+// indredientsListEl.append(...markingList(ingredients));
