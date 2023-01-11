3c51bb0dfc603c8f1c0ccd8d7a1a928acd8be97c
Andrey Valenko <andreyvalenko@gmail.com>
2023-01-11 15:34:58 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyRef = document.querySelector("body");
+const btnSetColorRef = bodyRef.querySelector(".change-color");
+const colorValueRef = bodyRef.querySelector(".color");
+
+btnSetColorRef.addEventListener("click", changeColor);
+
+function changeColor() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  colorValueRef.textContent = bodyRef.style.backgroundColor;
+}
