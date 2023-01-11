87513b5ff8595b6f9d463c379376240dd3446e5f
Andrey Valenko <andreyvalenko@gmail.com>
2023-01-11 14:54:34 +0200

    Update task-08.js

---
+++ js/task-08.js
@@ -0,0 +1,23 @@
+// const formData = {};
+
+// const formEl = document.querySelector(".login-form");
+
+// formEl.addEventListener("submit", handleSubmit);
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const {
+//     elements: { email, password },
+//   } = event.currentTarget;
+
+//   if (email.value === "" && password.value === "") {
+//     return alert("Всі поля мають бути заповнені");
+//   }
+
+//   formData[email.getAttribute("name")] = email.value;
+//   formData[password.getAttribute("name")] = password.value;
+//   console.log(formData);
+//   event.currentTarget.reset();
+// }
+
+const m = [];
