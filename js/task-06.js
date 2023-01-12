0711b670b6624f606f412d01275acfb1814373cc
Andrey Valenko <andreyvalenko@gmail.com>
2023-01-12 10:52:06 +0200

    s

--- js/task-06.js
+++ js/task-06.js
@@ -5,7 +5,7 @@ textInput.addEventListener("blur", () => {
     textInput.classList.add("valid");
     textInput.classList.remove("invalid");
   } else {
-    textInput.classList.remove("valid");
     textInput.classList.add("invalid");
+    textInput.classList.remove("valid");
   }
 });
--- js/task-10.js
+++ js/task-10.js
@@ -3,27 +3,28 @@ function getRandomHexColor() {
 }
 
 const controlsRef = document.getElementById("controls");
-const inputRef = document.getElementById("controls").firstElementChild;
+const inputRef = controlsRef.firstElementChild;
 const boxesRef = document.getElementById("boxes");
 
-const inputValue = [];
+let boxQuantity;
 
 inputRef.addEventListener("input", (event) => {
-  console.log(createBoxes(event.currentTarget.value));
+  boxQuantity = event.currentTarget.value;
 });
 
-console.log(inputValue);
-function createBoxes(amount) {
-  const elSizeValue = [];
-  for (let i = 30; i <= 30 + (amount - 1) * 10; i += 10) {
-    elSizeValue.push(i);
-  }
-  return elSizeValue.map((el) => {
-    const boxEl = document.createElement("div");
-    boxEl.style.width = `${el}px`;
-    boxEl.style.height = `${el}px`;
-    boxEl.style.backgroundColor = getRandomHexColor();
-    return boxEl;
-  });
-}
-boxesRef.append(...createBoxes(5));
+console.log(boxQuantity);
+
+// function createBoxes(amount) {
+//   const elSizeValue = [];
+//   for (let i = 30; i <= 30 + (amount - 1) * 10; i += 10) {
+//     elSizeValue.push(i);
+//   }
+//   return elSizeValue.map((el) => {
+//     const boxEl = document.createElement("div");
+//     boxEl.style.width = `${el}px`;
+//     boxEl.style.height = `${el}px`;
+//     boxEl.style.backgroundColor = getRandomHexColor();
+//     return boxEl;
+//   });
+// }
+// boxesRef.append(...createBoxes(5));
