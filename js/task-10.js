f221397c963c42eae04be73ce64b3dda63170e30
Andrey Valenko <andreyvalenko@gmail.com>
2023-01-12 12:34:12 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,30 +1,41 @@
+const controlsRef = document.getElementById("controls");
+const inputRef = controlsRef.firstElementChild;
+const onCreateBtn = controlsRef.querySelector("[data-create]");
+const onDestroyBtn = controlsRef.querySelector("[data-destroy]");
+const boxesRef = document.getElementById("boxes");
+
+let outputValue;
+
+inputRef.addEventListener("blur", (event) => {
+  outputValue = event.currentTarget.value;
+});
+
+onCreateBtn.addEventListener("click", () =>
+  boxesRef.append(...createBoxes(outputValue))
+);
+
+onDestroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  const elSizeValue = [];
+
+  for (let i = 30; i <= 30 + (amount - 1) * 10; i += 10) {
+    elSizeValue.push(i);
+  }
+
+  return elSizeValue.map((el) => {
+    const boxEl = document.createElement("div");
+    boxEl.style.width = `${el}px`;
+    boxEl.style.height = `${el}px`;
+    boxEl.style.backgroundColor = getRandomHexColor();
+    return boxEl;
+  });
+}
+
+function destroyBoxes() {
+  boxesRef.innerHTML = "";
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
-const controlsRef = document.getElementById("controls");
-const inputRef = controlsRef.firstElementChild;
-const boxesRef = document.getElementById("boxes");
-
-let boxQuantity;
-
-inputRef.addEventListener("input", (event) => {
-  boxQuantity = event.currentTarget.value;
-});
-
-console.log(boxQuantity);
-
-// function createBoxes(amount) {
-//   const elSizeValue = [];
-//   for (let i = 30; i <= 30 + (amount - 1) * 10; i += 10) {
-//     elSizeValue.push(i);
-//   }
-//   return elSizeValue.map((el) => {
-//     const boxEl = document.createElement("div");
-//     boxEl.style.width = `${el}px`;
-//     boxEl.style.height = `${el}px`;
-//     boxEl.style.backgroundColor = getRandomHexColor();
-//     return boxEl;
-//   });
-// }
-// boxesRef.append(...createBoxes(5));
