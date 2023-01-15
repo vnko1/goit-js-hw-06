079bf9f5b1d50f45924ed65031d5cb25a1771b8f
Andrey Valenko <andreyvalenko@gmail.com>
2023-01-15 15:55:35 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -5,6 +5,7 @@ const onDestroyBtn = controlsRef.querySelector("[data-destroy]");
 const boxesRef = document.getElementById("boxes");
 
 let outputValue;
+let boxes;
 
 inputRef.addEventListener("input", (event) => {
   outputValue = event.currentTarget.value;
@@ -14,6 +15,7 @@ onCreateBtn.addEventListener("click", () => {
   outputValue === undefined
     ? alert("Введіть дані")
     : boxesRef.append(...createBoxes(outputValue));
+  boxes = boxesRef.querySelectorAll("div");
 });
 
 onDestroyBtn.addEventListener("click", destroyBoxes);
@@ -21,8 +23,19 @@ onDestroyBtn.addEventListener("click", destroyBoxes);
 function createBoxes(amount) {
   const elSizeValue = [];
 
-  for (let i = 30; i <= 30 + (amount - 1) * 10; i += 10) {
-    elSizeValue.push(i);
+  if (boxes !== undefined) {
+    const initialValue = Number.parseInt(boxes[boxes.length - 1].style.width);
+    for (
+      let i = initialValue + 10;
+      i <= initialValue + 10 + (amount - 1) * 10;
+      i += 10
+    ) {
+      elSizeValue.push(i);
+    }
+  } else {
+    for (let i = 30; i <= 30 + (amount - 1) * 10; i += 10) {
+      elSizeValue.push(i);
+    }
   }
 
   return elSizeValue.map((el) => {
@@ -36,6 +49,7 @@ function createBoxes(amount) {
 
 function destroyBoxes() {
   boxesRef.innerHTML = "";
+  boxes = undefined;
 }
 
 function getRandomHexColor() {
