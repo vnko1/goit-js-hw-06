253cdb5cea4d99b06dbe523c19ed67d34e586ee5
Andrey Valenko <andreyvalenko@gmail.com>
2023-01-19 10:59:04 +0200

    s

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,20 @@
+// const textInput = document.querySelector("#name-input");
+// const textOutput = document.querySelector("#name-output");
+
+// textInput.addEventListener("input", (event) => {
+
+//   event.currentTarget.value === ""
+//     ? (textOutput.textContent = "Anonymous")
+//     : (textOutput.textContent = event.currentTarget.value);
+// });
+
 const textInput = document.querySelector("#name-input");
 const textOutput = document.querySelector("#name-output");
 
-textInput.addEventListener("input", (event) => {
-  event.currentTarget.value === ""
+textInput.addEventListener("input", _.debounce(onInputData, 250));
+
+function onInputData(event) {
+  event.target.value === ""
     ? (textOutput.textContent = "Anonymous")
-    : (textOutput.textContent = event.currentTarget.value);
-});
+    : (textOutput.textContent = event.target.value);
+}
